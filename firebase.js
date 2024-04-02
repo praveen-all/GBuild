50388032f4775ca04e6f518f269404be2892aff7
unnihc <unnihc369@gmail.com>
2024-04-02 19:19:12 +0530

    Added routes

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./key.json'); // Replace with actual path
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+module.exports = admin;
