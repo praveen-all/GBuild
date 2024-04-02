c6b2ee464c92a39fbbe96406a78a81ad88a7e34a
Praveen Kumar G <praveenkumargpraveenkumarg85@gmail.com>
2024-04-03 00:21:53 +0530

    changes testscore

--- controllers/testScoreController.js
+++ controllers/testScoreController.js
@@ -1,49 +1,52 @@
 const admin = require("../firebase");
 
-const testCollection = admin.firestore().collection("testScore"); // Access users 
+const testCollection = admin.firestore().collection("testScore"); // Access users
 
-const addTheTest=async(req,res)=>{
-  const body=req.body;
-    try {
-        const add=await testCollection.add(body);
-    
-        res.status(200).json({
-          status: "success",
-          data: add,
-        });
-        
-    } catch (error) {
-         res.status(400).json({
-            status:"error",
-            message:error.message
-         });
-    }
-}
+const addTheTest = async (req, res) => {
+  const body = req.body;
+  body["userId"] = req.userId;
+  try {
+    const id = (await testCollection.add(body)).id;
+    const add = (await testCollection.doc(id).get()).data();
 
-const getSemTest=async(req,res)=>{
-    const {sem}=req.body;
+    res.status(200).json({
+      status: "success",
+      data: add,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
 
-     if(!sem){
-       return res.status(401).json({
-            status:"fails",
-            message:"please provide the sem & user"
-        })
-     }
-    try {
-        const getallTest=await testCollection.where('userId','==',req.userId).where('sem','==',sem).get();
-            const documents = getallTest.docs.map((doc) => doc.data());
-           
-        res.status(200).json({
-          status: "success",
-          data: documents,
-        });
-    } catch (error) {
-        res.status(400).json({
-            status:"error",
-            message:error.message
-        })
-    }
-}
+const getSemTest = async (req, res) => {
+  const { sem } = req.body;
 
+  if (!sem) {
+    return res.status(401).json({
+      status: "fails",
+      message: "please provide the sem & user",
+    });
+  }
+  try {
+    const getallTest = await testCollection
+      .where("userId", "==", req.userId)
+      .where("sem", "==", sem)
+      .get();
+    const documents = getallTest.docs.map((doc) => doc.data());
+
+    res.status(200).json({
+      status: "success",
+      data: documents,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
 
 module.exports = { getSemTest, addTheTest };
--- routes/testScoreRoute.js
+++ routes/testScoreRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { getSemTest,addTheTest } = require("../controllers/testScoreController.js");
-const {protect}=require('./../middleware/authMiddleware.js')
+const {
+  getSemTest,
+  addTheTest,
+} = require("../controllers/testScoreController.js");
+const { protect } = require("./../middleware/authMiddleware.js");
 const router = express.Router();
 
-router.get("/getSemTest",protect, getSemTest);
-router.post("/add",addTheTest );
+router.get("/getSemTest", protect, getSemTest);
+router.post("/add", protect, addTheTest);
 
 module.exports = router;
